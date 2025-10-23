83509ed8cccd723d4c0beff4b5fcdb4471f2d112
unknown <haris.imamovic@stu.ibu.edu.ba>
2025-10-23 19:33:56 +0200

    Fixing documentation issues.

--- voting-system/backend/public/v1/docs/swagger.php
+++ voting-system/backend/public/v1/docs/swagger.php
@@ -2,6 +2,9 @@
 
 require __DIR__ . '/../../../vendor/autoload.php';
 
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
 if($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == '127.0.0.1'){
     define('BASE_URL', 'http://localhost:80/Web-Programming/voting-system/backend/');
 } else {
